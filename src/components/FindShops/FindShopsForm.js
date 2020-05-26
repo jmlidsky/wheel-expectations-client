eadbb934c1fd2240378259df8d674c3c739d5c2a
Jill Lidsky <jilllidsky15@gmail.com>
2020-05-26 15:58:10 -0400

    changed placeholder text

--- src/components/FindShops/FindShopsForm.js
+++ src/components/FindShops/FindShopsForm.js
@@ -74,7 +74,7 @@ class FindShopsForm extends Component {
                         type="text"
                         name="find-shops"
                         className="find-shops-input"
-                        placeholder="City, State, or Zip"
+                        placeholder="City, State, or Zip Code"
                         onChange={this.handleLocationChange}
                         required
                     />
